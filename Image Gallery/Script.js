b9158dd2ee1683c0537d0d207ad6ed0de57e7b44
Nada Abdallah <130399171+nadabdallah@users.noreply.github.com>
2024-08-29 15:20:04 +0300

    Add files via upload

---
+++ Image Gallery/Script.js
@@ -0,0 +1,9 @@
+var Full=document.getElementById("Full");
+var Full_img=document.getElementById("Full_img");
+function openimage(pic){
+    Full.style.display="flex";
+    Full_img.src=pic;
+}
+function closingimage(){
+    Full.style.display="none";
+}
